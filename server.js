b8b94514cf7d7e74b99741834dfa1f25636d0c81
fedemendez2014 <fedemendez2014@hotmail.com>
2020-04-22 01:43:47 -0300

    ahora si terminado con la integración con socket.io

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express'); // esto es igual a import express from 'express';
+const app = express(); // aca inicializo express
+const server = require('http').Server(app);
+
+const cors = require('cors'); // para control del cors
 const bodyParser = require('body-parser'); //require al body parser
+const socket = require('./socket');
 const db = require('./db');
 const router = require('./network/routes');
 
 db('mongodb+srv://usermongo:usermongo@telegrom-xdxov.mongodb.net/test');
-var app = express(); // aca inicializo express
+
+app.use(cors());
 app.use(bodyParser.json()); //agrego el body parser para porder utilizarlo
+
+socket.connect(server);
 router(app);
 
 app.use('/app', express.static('public')); //Sirve archivos estaticos como html, css, etc
 
-app.listen(3000); //puerto en el que escucha
-console.log('Aplicacion escuchando en http://localhost:3000');
+server.listen(3000, function () {
+    console.log('Aplicacion escuchando en http://localhost:3000');
+}); //puerto en el que escucha
